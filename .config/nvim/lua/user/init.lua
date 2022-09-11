a5c5e4faa5ef2cb35259962a97eab505365a9e16
Devon Mar <devonm@mdmm.ca>
2022-09-10 17:26:11 -0700

    Add AstroNVim user config

---
+++ .config/nvim/lua/user/init.lua
@@ -0,0 +1,24 @@
+local config = {
+  plugins = {
+    init = {
+      ["declancm/cinnamon.nvim"] = { disable = true }
+    }
+  },
+  highlights = {
+    default_theme = function(highlights)
+      local C = require "default_theme.colors"
+
+      -- transparency
+      -- https://github.com/AstroNvim/AstroNvim/issues/8
+      highlights.Normal = {bg = C.none, ctermbg = C.none}
+      highlights.CursorColumn = {cterm = {}, ctermbg = C.none, ctermfg = C.none}
+      highlights.CursorLineNr = {cterm = {}, ctermbg = C.none, ctermfg = C.none}
+      highlights.LineNr = {}
+      highlights.SignColumn = {}
+      highlights.StatusLine = {}
+      return highlights
+    end,
+  },
+}
+
+return config
