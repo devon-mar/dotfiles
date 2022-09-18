857c0892accb7568ea556ac7bfcbb8a8c08e0a63
Devon Mar <devonm@mdmm.ca>
2022-09-17 19:50:21 -0700

    Update nvim

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -1,7 +1,8 @@
 local config = {
   plugins = {
     init = {
-      ["declancm/cinnamon.nvim"] = { disable = true }
+      ["declancm/cinnamon.nvim"] = { disable = true },
+      ["goolord/alpha-nvim"] = { disable = true }
     },
     notify = {
       background_colour = "#000000"
@@ -13,15 +14,23 @@ local config = {
 
       -- transparency
       -- https://github.com/AstroNvim/AstroNvim/issues/8
-      highlights.Normal = {bg = C.none, ctermbg = C.none}
-      highlights.CursorColumn = {cterm = {}, ctermbg = C.none, ctermfg = C.none}
-      highlights.CursorLineNr = {cterm = {}, ctermbg = C.none, ctermfg = C.none}
-      highlights.LineNr = {}
-      highlights.SignColumn = {}
-      highlights.StatusLine = {}
+      if vim.g.neovide == nil then
+        highlights.Normal = {bg = C.none, ctermbg = C.none}
+        highlights.CursorColumn = {cterm = {}, ctermbg = C.none, ctermfg = C.none}
+        highlights.CursorLineNr = {cterm = {}, ctermbg = C.none, ctermfg = C.none}
+        highlights.LineNr = {}
+        highlights.SignColumn = {}
+        highlights.StatusLine = {}
+      end
       return highlights
     end,
   },
+  polish = function()
+    if vim.g.neovide == true then
+      vim.g.neovide_cursor_animation_length = 0
+      vim.opt.guifont = "CaskaydiaMono NF SemiLight:h12"
+    end
+  end
 }
 
 return config
