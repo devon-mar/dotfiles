58f6d9e970f174cf5780ce91d2dc3382e2155f3c
Devon Mar <devonm@mdmm.ca>
2023-04-05 17:21:15 -0700

    Run PackerCompile automatically

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -12,7 +12,17 @@ end
 local packer_bootstrap = ensure_packer()
 
 return require("packer").startup(function(use)
-  use "wbthomason/packer.nvim"
+  use {
+    "wbthomason/packer.nvim",
+    config = function()
+      vim.cmd([[
+        augroup packer_user_config
+          autocmd!
+          autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+        augroup end
+      ]])
+    end
+  }
 
   use {
     "rcarriga/nvim-notify",
