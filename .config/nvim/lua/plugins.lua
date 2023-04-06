baf727d7db7a8850356981e604a8eee2045db399
Devon Mar <devonm@mdmm.ca>
2023-04-05 18:04:21 -0700

    Update cmp mappings

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -156,6 +156,8 @@ return require("packer").startup(function(use)
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
       vim.opt.completeopt = {"menu", "menuone", "noselect"}
 
       local has_words_before = function()
@@ -197,6 +199,11 @@ return require("packer").startup(function(use)
         TypeParameter = ""
       }
 
+      local function has_words_before()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+      end
+
       cmp.setup({
         formatting = {
           format = function(entry, vim_item)
@@ -206,9 +213,15 @@ return require("packer").startup(function(use)
               nvim_lsp = "[LSP]",
               luasnip = "[LuaSnip]",
               nvim_lua = "[Lua]",
-                latex_symbols = "[LaTeX]",
+              latex_symbols = "[LaTeX]",
             })[entry.source.name]
             return vim_item
+          end,
+        },
+        snippet = {
+          -- REQUIRED - you must specify a snippet engine
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
           end,
         },
         window = {
@@ -219,32 +232,32 @@ return require("packer").startup(function(use)
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-e>'] = cmp.mapping.abort(),
-          ['<C-Space>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
-          },
-          ['<Tab>'] = function(fallback)
-            if not cmp.select_next_item() then
-              if vim.bo.buftype ~= 'prompt' and has_words_before() then
-                cmp.complete()
-              else
-                fallback()
-              end
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then cmp.complete()
+            else
+              fallback()
             end
-          end,
-          ['<S-Tab>'] = function(fallback)
-            if not cmp.select_prev_item() then
-              if vim.bo.buftype ~= 'prompt' and has_words_before() then
-                cmp.complete()
-              else
-                fallback()
-              end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
             end
-          end,
+          end, { "i", "s" }),
+          ["<CR>"] = cmp.mapping.confirm { select = false },
         }),
         sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "buffer" },
+          { name = "nvim_lsp", priority = 30 },
+          { name = "buffer", priority = 20 },
+          { name = "path", priority = 10 },
         }),
         -- https://github.com/hrsh7th/nvim-cmp/pull/676#issuecomment-1002532096
         enabled = function()
@@ -260,6 +273,7 @@ return require("packer").startup(function(use)
   }
   use "hrsh7th/cmp-nvim-lsp"
   use "hrsh7th/cmp-buffer"
+  use "hrsh7th/cmp-path"
 
   use {
     "luochen1990/rainbow",
@@ -385,6 +399,8 @@ return require("packer").startup(function(use)
       require("nvim-autopairs").setup()
     end,
   }
+
+  use "L3MON4D3/LuaSnip"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
