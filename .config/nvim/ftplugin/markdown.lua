960c29fdf521c7126d4b8ccccc8afb7a89561588
Devon Mar <devonm@mdmm.ca>
2023-10-07 18:47:55 -0700

    Make autocmd buffer local

--- .config/nvim/ftplugin/markdown.lua
+++ .config/nvim/ftplugin/markdown.lua
@@ -5,6 +5,7 @@ vim.api.nvim_buf_create_user_command(0, "PandocEnable", function()
   vim.api.nvim_clear_autocmds({ group = augroup })
   vim.api.nvim_create_autocmd("BufWritePost", {
     group = augroup,
+    buffer = 0,
     callback = function()
       local output = vim.fn.system({ "pandoc", vim.fn.expand("%:p"), "-o", vim.fn.expand("%:p:r") .. ".pdf" })
       vim.notify("pandoc exited with code " .. vim.v.shell_error .. ": " .. output)
